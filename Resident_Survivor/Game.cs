445b6afeafb8a408efe9f6f8bff84721e2be5ef6
Kasper K <kakr@iba.dk>
2022-10-13 16:57:00 +0200

    Implementing basic game loop

---
+++ Resident_Survivor/Game.cs
@@ -0,0 +1,46 @@
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Resident_Survivor {
+    class Game
+    {
+        private const int WIDTH = 640;
+        private const int HEIGHT = 480;
+        private const string TITLE = "Resident Survivo";
+        private RenderWindow window;
+        private VideoMode mode = new VideoMode(WIDTH, HEIGHT);
+        
+        public Game()
+        {
+            this.window = new RenderWindow(this.mode, TITLE);
+            
+            this.window.SetVerticalSyncEnabled(true);
+            this.window.Closed += (sender, args) => {
+                this.window.Close(); 
+            };
+        }
+
+        public void run() 
+        {
+            while(this.window.IsOpen)
+            {
+                this.handleEvents();
+                this.update();
+                this.draw();            
+            }
+        }
+
+        private void handleEvents()
+        {
+            this.window.DispatchEvents();
+        }
+        private void update() {}
+
+        private void draw() 
+        {
+            this.window.Clear(Color.Blue);
+            this.window.Display();
+        }
+        
+    }
+}
