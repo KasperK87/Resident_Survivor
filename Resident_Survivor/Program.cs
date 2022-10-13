3c580297a54df3a3783a7c359be96b73372dcceb
Kasper K <kakr@iba.dk>
2022-10-13 16:17:42 +0200

    C# SFML implemented

---
+++ Resident_Survivor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Resident_Survivor
+{
+    class Program
+    {
+        const int WIDTH = 640;
+        const int HEIGHT = 480;
+        const string TITLE = "Resident_Survivor";
+        static void Main(string[] args)
+        {
+            VideoMode mode = new VideoMode(WIDTH, HEIGHT);
+            RenderWindow window = new RenderWindow(mode, TITLE);
+            
+            window.SetVerticalSyncEnabled(true);
+
+            window.Closed += (sender, args) => window.Close();
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+                window.Clear(Color.Red);
+                window.Display();
+            }
+        }
+    }
+}
